4d4628c3f59fa60b6acb1ebb0bef6191b538ea5a
daniel-yl-fan <daniel_qdcn@hotmail.com>
2020-01-05 04:49:42 -0500

    cpp

--- OpenFileToRead/read_line_from_file.cpp
+++ OpenFileToRead/read_line_from_file.cpp
@@ -9,7 +9,7 @@ string& read_line_from_file( const char* fileName){
     static ifstream file;
     if( file.is_open() == false){
         file.open( fileName);
-    } 
+    }
     if( file.is_open() == true){
         static string line;
         getline( file, line); 
