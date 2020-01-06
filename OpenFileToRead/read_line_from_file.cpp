3955eba5c2f67119946419a7714c7fe6f1a292ea
daniel-yl-fan <daniel_qdcn@hotmail.com>
2020-01-06 11:56:38 -0500

    cpp

--- OpenFileToRead/read_line_from_file.cpp
+++ OpenFileToRead/read_line_from_file.cpp
@@ -1,18 +1,17 @@
-#include <iostream>
-using std::cout;
-using std::endl;
 #include <fstream>
-using std::ifstream;
 #include "read_line_from_file.h"
 
-string& read_line_from_file( const char* fileName){
-    static ifstream file;
-    if( file.is_open() == false){
-        file.open( fileName);
+string& read_line_from_file( const char* fileName)
+{
+    static std::ifstream file;
+    if( file.is_open() == false)
+    {
+        file.open(fileName);
     }
-    if( file.is_open() == true){
+    if( file.is_open() == true)
+    {
         static string line;
-        getline( file, line); 
+        getline(file, line);
         return line;
     }
 }
--- OpenFileToRead/test_read_line_from_file.cpp
+++ OpenFileToRead/test_read_line_from_file.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
-using std::cout;
-using std::endl;
 #include "read_line_from_file.h"
 int main(){
-    cout << read_line_from_file( "/home/dafan/public_html/c++/open_file_to_read/makefile") << endl;
-    cout << read_line_from_file( "/home/dafan/public_html/c++/open_file_to_read/makefile") << endl;
-    cout << read_line_from_file( "/home/dafan/public_html/c++/open_file_to_read/makefile") << endl;
-    cout << read_line_from_file( "/home/dafan/public_html/c++/open_file_to_read/makefile") << endl;
-    cout << read_line_from_file( "/home/dafan/public_html/c++/open_file_to_read/makefile") << endl;
-    cout << read_line_from_file( "/home/dafan/public_html/c++/open_file_to_read/makefile") << endl;
-    cout << read_line_from_file( "/home/dafan/public_html/c++/open_file_to_read/makefile") << endl;
-    cout << read_line_from_file( "/home/dafan/public_html/c++/open_file_to_read/makefile") << endl;
+    std::cout << read_line_from_file( "/home/dafan/public_html/c++/open_file_to_read/makefile") << std::endl;
+    std::cout << read_line_from_file( "/home/dafan/public_html/c++/open_file_to_read/makefile") << std::endl;
+    std::cout << read_line_from_file( "/home/dafan/public_html/c++/open_file_to_read/makefile") << std::endl;
+    std::cout << read_line_from_file( "/home/dafan/public_html/c++/open_file_to_read/makefile") << std::endl;
+    std::cout << read_line_from_file( "/home/dafan/public_html/c++/open_file_to_read/makefile") << std::endl;
+    std::cout << read_line_from_file( "/home/dafan/public_html/c++/open_file_to_read/makefile") << std::endl;
+    std::cout << read_line_from_file( "/home/dafan/public_html/c++/open_file_to_read/makefile") << std::endl;
+    std::cout << read_line_from_file( "/home/dafan/public_html/c++/open_file_to_read/makefile") << std::endl;
 }
